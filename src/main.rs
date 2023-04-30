88d1582ae1df3e54fa6ca4ce5268dff8872c88d2
Guy Marshall <guymarshall9@gmail.com>
2023-04-30 09:28:22 +0100

    Make code more readable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    const H: &str = "H";
+    const E: &str = "e";
+    const L: &str = "l";
+    const O: &str = "o";
+    const COMMA: &str = ",";
+    const SPACE: &str = " ";
+    const W: &str = "w";
+    const R: &str = "r";
+    const D: &str = "d";
+    const EXCLAMATION_POINT: &str = "!";
+
+    println!("{}{}{}{}{}{}{}{}{}{}{}{}{}", H, E, L, L, O, COMMA, SPACE, W, O, R, L, D, EXCLAMATION_POINT);
 }
